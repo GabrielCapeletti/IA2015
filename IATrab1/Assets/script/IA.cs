84a9be25086e7b3c622e2a2f21d4d09dd1d69414
GabrielCapeletti <capela898@hotmail.com>
2015-09-01 00:38:47 -0300

    Primeira entraga

--- IATrab1/Assets/script/IA.cs
+++ IATrab1/Assets/script/IA.cs
@@ -7,6 +7,7 @@ namespace Assets.script
     public class IA : MonoBehaviour
     {
         private bool _firstPlay;
+        private Nodo _node;
         private void Start()
         {
             _firstPlay = true;
@@ -17,12 +18,18 @@ namespace Assets.script
             if (_firstPlay)
             {
                 FirstPlay();
-                _firstPlay = false;
+               // _firstPlay = false;
                 return;
             }
 
+            //Se a jogada do jagador estiver na previsão da ia n cria nova arvore, 
+            //utiliza o filho com o estado correspondente
 
-            Nodo n = new Nodo(Game.Instance.Board, true, null);
+            _node = new Nodo(Game.Instance.Board, true, null, new Vector2(-1, -1));
+            
+
+            Vector2 move = _node.StartConstructionAndSearch();
+            Game.Instance.EndComputerTurn((int)move.x, (int)move.y);
         }
 
         public void FirstPlay()
--- IATrab1/Assets/script/Nodo.cs
+++ IATrab1/Assets/script/Nodo.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.script
 {
+    /**
+     * Nodo representantando cada estado.
+     * O estado do tabuleiro é representado por uma matrix de inteiros
+     * @ Gabriel Capeletti 
+     */
     class Nodo
     {
 
@@ -14,42 +20,81 @@ namespace Assets.script
         private float   _value;
         private int[][] _board;
         private bool    _isMaxNode;
-    
-        public Nodo(int[][] board, bool isMaxNode, Nodo parent)
+        private Vector2 _chosenMove;
+        private Vector2 _originalMove;
+        
+
+        private List<Vector2> _testedPlaces; 
+        public Nodo(int[][] board, bool isMaxNode, Nodo parent,Vector2 original)
         {
 
-            _value      = (_isMaxNode) ? -10000000 : 10000000;
-            _alfa       = -10000000;
-            _board      = board;
-            _isMaxNode  = isMaxNode;
-            _parent     = parent;
-
-            StartConstructionAndSearch();
+            _value        = (_isMaxNode) ? -10000000 : 10000000;
+            _alfa         = -10000000;
+            _board        = board;
+            _isMaxNode    = isMaxNode;
+            _parent       = parent;
+            _originalMove = original;
         }
 
-        private void StartConstructionAndSearch()
+        //Inicia busca por profundidade mais a esq/
+        public Vector2 StartConstructionAndSearch()
         {
+            CreateChild();
+
+            return _chosenMove;
+        }
+
+        private void CreateChild()
+        {
+            Vector2 move = ChooseNextOption();
+            int[][] newBoard = _board;
+            _board[(int)move.x][(int)move.y] = 1;
+
+            Nodo filho = new Nodo(newBoard, !_isMaxNode, this, move);
+
+            //if (heuristica De Continuacao e de Poda)
+            //    CreateChild();
+            //else Obtem um valor total baseado no maior ponto possível no ponto play
+            //  se for max considera as peças brancas [ 0, 5]
+            //  se for min considera as peças pretas  [-5, 0]
             
+            //Se n for raiz
+            if (_parent != null)
+            {
+                if (_isMaxNode)
+                {
+                    _parent.SetBeta(_value,move);
+                    return;
+                }
+                _parent.SetAlfa(_value,move);
+            }
+
         }
 
-        public void SetBeta(float beta)
+        //Heuristica de escolha da jogada retornando posição a testar
+        private Vector2 ChooseNextOption()
         {
-            _beta = beta;
+            return new Vector2(1, 1);
+        }
 
+        public void SetBeta(float beta,Vector2 newMove)
+        {
             if (!_isMaxNode)
                 if (_value < beta)
+                {
                     _value = beta;
+                    _chosenMove = newMove;
+                }
         }
 
-        public void SetAlfa(float alfa)
+        public void SetAlfa(float alfa, Vector2 newMove)
         {
-            _alfa = alfa;
-
             if (_isMaxNode)
                 if (_value < alfa)
+                {
                     _value = alfa;
-                
-            
+                    _chosenMove = newMove;
+                }
         }
 
         public float Beta
